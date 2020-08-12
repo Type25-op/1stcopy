f8e43d334bd597fa684213d8fbfeb97b44b0fc18
Type25-op <69530961+Type25-op@users.noreply.github.com>
2020-08-12 23:36:07 +0530

    made

---
+++ config/config.py
@@ -0,0 +1,6 @@
+__token__ = 'NjcyODY2NjA2MjU4MzIzNDU2.XjRt3A.oLFE4qXZZ-SorjFVljDPBK_XE2U'
+__prefix__ = ':' #OPTIONAL Prefix for all commands, defaults to colon
+__botserverid__ =  #OPTIONAL Specifies the main serverid from which the server-/modlog should be taken + some other nito features
+__kawaiichannel__ =  #OPTIONAL specified a channel where the :kawaii commands gets this pinned messages
+__greetmsg__ = '{emoji} Welcome {member} on my server!' #OPTIONAL sends a greet message to new user in the botserverid system channel
+__leavemsg__ = ':sad: {member} left the server' #OPTIONAL sends a leave message to the botserverid system channel
