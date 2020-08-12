a4a2398fdaed26f09e9239285c9d18bb2a408401
Type25-op <69530961+Type25-op@users.noreply.github.com>
2020-08-12 23:43:25 +0530

    update

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-__token__ = 'NjcyODY2NjA2MjU4MzIzNDU2.XjRt3A.oLFE4qXZZ-SorjFVljDPBK_XE2U'
+__token__ = 'NjcyODY2NjA2MjU4MzIzNDU2.XjRt3A.h60wCwroMuwrQ7YoaBjACGVbW7Q'
 __prefix__ = ':' #OPTIONAL Prefix for all commands, defaults to colon
 __botserverid__ =  #OPTIONAL Specifies the main serverid from which the server-/modlog should be taken + some other nito features
 __kawaiichannel__ =  #OPTIONAL specified a channel where the :kawaii commands gets this pinned messages
